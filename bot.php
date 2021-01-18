74968cc1e0ad941f27b0abed6cb905776d1ae727
Abhishek Panchmiya <abhishekpanchmiya1102@gmail.com>
2021-01-18 21:43:54 +0530

    Admission Chatbot project

--- bot.php
+++ bot.php
@@ -1,15 +1,15 @@
-<html>
-    <head>
+<!DOCTYPE html>
+<html lang="en">
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Simple Chatbot in PHP | CodingNepal</title>
     <link rel="stylesheet" href="style.css">
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    </head>
-
-    <body> 
-        <div class="wrapper">
+</head>
+<body>
+    <div class="wrapper">
         <div class="title">Simple Online Chatbot</div>
         <div class="form">
             <div class="bot-inbox inbox">
@@ -21,12 +21,12 @@
                 </div>
             </div>
         </div>
-            <div class="typing-field">
-                <div class="input-data">
-                    <input type="text" placeholer="Type Something here..." required>
-                    <button>Send</button>
-                </div>
+        <div class="typing-field">
+            <div class="input-data">
+                <input id="data" type="text" placeholder="Type something here.." required>
+                <button id="send-btn">Send</button>
             </div>
         </div>
-    </body>
+    </div>
+</body>
 </html>
