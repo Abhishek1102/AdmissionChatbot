4cad15aa14eafaa44282a780dc37c01748038206
Abhishek Panchmiya <abhishekpanchmiya1102@gmail.com>
2021-01-18 13:11:21 +0530

    admission bot project

---
+++ bot.php
@@ -0,0 +1,32 @@
+<html>
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple Chatbot in PHP | CodingNepal</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    </head>
+
+    <body> 
+        <div class="wrapper">
+        <div class="title">Simple Online Chatbot</div>
+        <div class="form">
+            <div class="bot-inbox inbox">
+                <div class="icon">
+                    <i class="fas fa-user"></i>
+                </div>
+                <div class="msg-header">
+                    <p>Hello there, how can I help you?</p>
+                </div>
+            </div>
+        </div>
+            <div class="typing-field">
+                <div class="input-data">
+                    <input type="text" placeholer="Type Something here..." required>
+                    <button>Send</button>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
